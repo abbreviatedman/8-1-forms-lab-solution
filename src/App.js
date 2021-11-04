9f604029dd4356d07324546d4db5396c70a022cc
Colin Jaffe <balloonasaurus@gmail.com>
2021-11-04 15:59:45 -0400

    Initial commit.

---
+++ src/App.js
@@ -0,0 +1,69 @@
+import React from "react";
+import Form from "./Form";
+import "./App.css";
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = { result: "No result yet.", error: false };
+  }
+
+  calculate = (calculation, values) => {
+    if (values.split(",").some((num) => isNaN(num))) {
+      this.setState({ result: "Invalid input.", error: true });
+
+      return;
+    }
+
+    const nums = values.split(",").map(Number);
+    switch (calculation) {
+      case "sum":
+        this.setState({
+          result: nums.reduce((sum, num) => sum + Number(num), 0),
+          error: false,
+        });
+
+        break;
+
+      case "average":
+        this.setState({
+          result: nums.reduce((sum, num) => sum + Number(num), 0) / nums.length,
+          error: false,
+        });
+
+        break;
+
+      case "mode":
+        const counts = {};
+        nums.forEach((num) => (counts[num] = counts[num] + 1 || 1));
+        this.setState({
+          result: Object.keys(counts).reduce(
+            (max, num) => (counts[num] > counts[max] ? num : max),
+            Object.keys(counts)[0]
+          ),
+
+          error: false,
+        });
+
+        break;
+
+      default:
+        this.setState({ result: "Please select an option.", error: true });
+        break;
+    }
+  };
+
+  render() {
+    return (
+      <main>
+        <p>Enter each number in the array, separated by a ','</p>
+        <Form handleSubmit={this.calculate} error={this.state.error} />
+        <section id="result">
+          <p>{this.state.result}</p>
+        </section>
+      </main>
+    );
+  }
+}
+
+export default App;
---
+++ src/Form.js
@@ -0,0 +1,70 @@
+import React from "react";
+import "./Form.css";
+
+class Form extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      calculation: "",
+      values: "",
+    };
+  }
+
+  handleInput = (event) => {
+    this.setState({ values: event.target.value });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    if (
+      !this.state.values ||
+      this.state.values.split(",").some((num) => isNaN(num))
+    ) {
+      this.props.handleSubmit(this.state.calculation, "a");
+
+      return;
+    }
+
+    this.props.handleSubmit(this.state.calculation, this.state.values);
+    if (this.state.calculation) {
+      this.setState({ values: "" });
+    }
+  };
+
+  handleSelect = (event) => {
+    this.setState({ calculation: event.target.value });
+  };
+
+  render() {
+    return (
+      <form>
+        <input
+          id="values"
+          name="values"
+          type="text"
+          value={this.state.values}
+          onChange={this.handleInput}
+          className={this.props.error ? "error" : ""}
+        />
+
+        <select
+          id="operation"
+          name="operation"
+          onChange={this.handleSelect}
+          className={this.props.error ? "error" : ""}
+        >
+          <option value=""></option>
+          <option value="sum">sum</option>
+          <option value="average">average</option>
+          <option value="mode">mode</option>
+        </select>
+
+        <button type="submit" onClick={this.handleSubmit}>
+          Calculate
+        </button>
+      </form>
+    );
+  }
+}
+
+export default Form;
